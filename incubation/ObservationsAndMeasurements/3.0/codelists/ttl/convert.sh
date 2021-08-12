fef74301f1290e84c018475405a0c46d3b72b26d
Gobe Hobona <ghobona@opengeospatial.org>
2021-08-12 19:04:58 +0100

    Incubation: Converted OMS 3.0 JSON-LD files to TTL

---
+++ incubation/ObservationsAndMeasurements/3.0/codelists/ttl/convert.sh
@@ -0,0 +1,6 @@
+/Users/ogckm/Tools/apache-jena-4.1.0/bin/riot --out=Turtle /Users/ogckm/Downloads/codelists/collectionType.jsonld > collectionType.ttl
+/Users/ogckm/Tools/apache-jena-4.1.0/bin/riot --out=Turtle /Users/ogckm/Downloads/codelists/collectionTypeByMemberCharacteristicsSemantics.jsonld > collectionTypeByMemberCharacteristicsSemantics.ttl
+/Users/ogckm/Tools/apache-jena-4.1.0/bin/riot --out=Turtle /Users/ogckm/Downloads/codelists/observationType.jsonld > observationType.ttl
+/Users/ogckm/Tools/apache-jena-4.1.0/bin/riot --out=Turtle /Users/ogckm/Downloads/codelists/observationTypeByResultType.jsonld > observationTypeByResultType.ttl
+/Users/ogckm/Tools/apache-jena-4.1.0/bin/riot --out=Turtle /Users/ogckm/Downloads/codelists/sampleType.jsonld > sampleType.ttl
+/Users/ogckm/Tools/apache-jena-4.1.0/bin/riot --out=Turtle /Users/ogckm/Downloads/codelists/sampleTypeByGeometryType.jsonld > sampleTypeByGeometryType.ttl
