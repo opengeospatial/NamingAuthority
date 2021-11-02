421a8a41a267e79c18aaa5713b3719c0ed2a4514
Piotr Zaborowski <pzaborowski@ogc.org>
2021-11-02 17:59:08 +0100

    fetch_gs_data.py dependencies

--- scripts/fetch_gs_data.py
+++ scripts/fetch_gs_data.py
@@ -6,11 +6,7 @@ from typing import List, Any
 import httpx
 import rdflib
 from rdflib.namespace import DC, DCTERMS, SKOS, OWL, RDF, RDFS, XSD, DCAT
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
+
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
