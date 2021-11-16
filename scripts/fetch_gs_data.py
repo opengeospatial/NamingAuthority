899e8aba78cf1b85f28d2ba8a570457ce7814905
Piotr Zaborowski <pzaborowski@ogc.org>
2021-11-16 13:39:10 +0100

    fetch script update

--- scripts/fetch_gs_data.py
+++ scripts/fetch_gs_data.py
@@ -5,6 +5,7 @@ from typing import List, Any
 import argparse
 import httpx
 import rdflib
+import os.path
 from rdflib.namespace import DC, DCTERMS, SKOS, OWL, RDF, RDFS, XSD, DCAT
 
 
@@ -26,43 +27,74 @@ def parse_spec_data():
     matrix = load_matrix(query, gs_range, gs_sheet_name)
 
     spec_uri = matrix[0][2].replace('"', '')
+    base_uri = matrix[1][2].replace('"', '')
     spec_uri_tokens = spec_uri.split("/")
     spec_id = spec_uri_tokens[len(spec_uri_tokens) - 1]
     g = init_graph()
     spec = rdflib.URIRef(spec_uri)
+    g.add((spec, RDF.type, rdflib.URIRef("http://www.opengis.net/def/ont/modspec/Specification")))
+    published = accepted = submitted = base_url = None
 
-    for r in matrix[1:]:
-        pred = rdflib.URIRef(r[1].replace('"', ''))
+    for r in matrix[2:]:
+        pred_uri = r[1].replace('"', '')
+        pred = rdflib.URIRef(pred_uri)
         obj = None
         obj = r[0].replace('"', '')
         if obj == "rdfs:Literal":
-            g.add((spec, pred, rdflib.Literal(r[2].replace('"', ''), datatype=RDFS.Literal)))
-        elif r[0] == "xsd:date":
+            g.add((spec, pred, rdflib.Literal(r[2].replace('"', '')))) #, datatype=RDFS.Literal)))
+        elif obj == "xsd:date":
             g.add((spec, pred, rdflib.Literal(r[2].replace('"', ''), datatype=XSD.date)))
         else:
             g.add((spec, pred, rdflib.URIRef(r[2].replace('"', ''))))
-    return spec_id, g
+
+        if pred_uri == 'http://purl.org/dc/terms/created':
+            base_url = r[2].replace('"', '')
+        if pred_uri == 'http://purl.org/dc/terms/dateSubmitted':
+            submitted = r[2].replace('"', '')
+        if pred_uri == 'http://purl.org/dc/terms/dateAccepted':
+            accepted = r[2].replace('"', '')
+        if pred_uri == 'http://www.opengis.net/def/ont/modspec/date':
+            published = r[2].replace('"', '')
+
+        if published is not None:
+            modified = published
+        elif accepted is not None:
+            modified = accepted
+        elif submitted is not None:
+            modified = submitted
+        else:
+            modified = None
+
+        if submitted is not None:
+            created = submitted
+        else:
+            created = None
+
+    return spec_id, g, base_uri, created, modified
 
 
 def parse_conf_classes(g):
-    query = "select D, B, C where D is not null"
-    gs_range = "B2:D"
+    query = "select E, B, C, D where E is not null"
+    gs_range = "B2:E"
     gs_sheet_name = "Conformance Classes"
 
     matrix = load_matrix(query, gs_range, gs_sheet_name)
+    conf_classes = []
 
     for r in matrix:
         conf_class = rdflib.URIRef(trim_citation(r[0]))
+        conf_classes.append(trim_citation(r[0]))
         g.add((conf_class, RDF.type, rdflib.URIRef("http://www.opengis.net/def/ont/modspec/ConformanceClass")))
         if trim_citation(r[1]) != "":
             g.add((conf_class, SKOS.prefLabel, rdflib.Literal(trim_citation(r[1]))))
         if trim_citation(r[2]) != "":
             g.add((conf_class, SKOS.definition, rdflib.Literal(trim_citation(r[2]))))
+        if trim_citation(r[3]) != "":
+            g.add((conf_class, DCAT.landingPage, rdflib.Literal(trim_citation(r[3]))))
+    return g, conf_classes
 
-    return g
 
-
-def parse_conf_relations(g):
+def parse_conf_relations(g, conf_classes):
     query = "select D, E, F where D is not null and E is not null and F is not null"
     gs_range = "D2:F"
     gs_sheet_name = "ConfClasses relations"
@@ -74,8 +106,18 @@ def parse_conf_relations(g):
                rdflib.URIRef(trim_citation(r[1])),
                rdflib.URIRef(trim_citation(r[2]))
                ))
+        if trim_citation(r[1]) == 'http://purl.org/dc/terms/hasPart':
+            conf_classes.remove(trim_citation(r[2]))
+        if trim_citation(r[1]) == 'http://www.opengis.net/def/ont/specrel/dependency':
+            try:
+                base_url1 = r[0].split("/conf/")[0]
+                base_url2 = r[2].split("/conf/")[0]
+                if base_url1 == base_url2 and conf_classes.__contains__(trim_citation(r[0])):
+                    conf_classes.remove(trim_citation(r[0]))
+            except Exception:
+                print("cannot remove class with dependencies from the root level: " + r[0])
 
-    return g
+    return g, conf_classes
 
 
 def trim_citation(s):
@@ -119,24 +161,86 @@ def load_matrix(query, gs_range, gs_sheet_name):
     return csv2array(csv)
 
 
-def main():
-    spec_id, g = parse_spec_data()
-    g = parse_conf_classes(g)
-    g = parse_conf_relations(g)
+def load_predicates_for_deduplication():
+    query = "select B where D is not null and E is not null and F is not null"
+    gs_range = "D2:F"
+    gs_sheet_name = "ConfClasses relations"
 
-    print("Graph: " + str(g.serialize(format="turtle")))
-    
+'''
+<http://www.opengis.net/spec/docs/16-107r2-anno> a owl:Ontology .
+
+<http://www.opengis.net/spec/landinfra/part7/1.0> a skos:ConceptScheme ;
+  dct:source <http://www.opengis.net/def/docs/16-107r2> ;
+  skos:prefLabel "Specification elements for OGC 16-107r2 LandInfra" ;
+  skos:definition "A convenience hierarchy for navigating the elements of a specification using the SKOS model" ;
+  dct:modified "2017-06-02"^^xsd:date ;
+  dct:created "2017-03-24"^^xsd:date ;
+  skos:hasTopConcept <http://www.opengis.net/spec/landinfra/part7/1.0/conf/landdivision> ,
+    <http://www.opengis.net/spec/landinfra/part7/1.0/conf/condominium> ;
+.
+'''
+
+def add_schema(g, spec_id, base_url, date_created, date_modified, top_conf_classes):
+    g.add((rdflib.URIRef('http://www.opengis.net/spec/docs/' + spec_id + '-anno'), RDF.type, OWL.Ontology))
+    scheme = rdflib.URIRef(base_url)
+    spec = rdflib.URIRef('http://www.opengis.net/def/docs/' + spec_id)
+    g.add((scheme, RDF.type, SKOS.ConceptScheme))
+    g.add((scheme, DCTERMS.source, spec))
+    g.add((scheme, SKOS.prefLabel, rdflib.Literal("Specification elements for " + spec_id)))
+    g.add((scheme, SKOS.definition, rdflib.Literal("A convenience hierarchy for navigating the elements of a specification using the SKOS model")))
+
+    if date_modified is not None:
+        g.add((scheme, DCTERMS.modified, rdflib.Literal(date_modified, datatype=XSD.date)))
+    if date_created is not None:
+        g.add((scheme, DCTERMS.created, rdflib.Literal(date_created, datatype=XSD.date)))
+    if top_conf_classes is not None:
+        for cc in top_conf_classes:
+            g.add((scheme, SKOS.hasTopConcept, rdflib.URIRef(cc)))
+
+def main(replace):
+    spec_id, g, base_url, created, modified = parse_spec_data()
+    g, conf_classes = parse_conf_classes(g)
+    g, relations = parse_conf_relations(g, conf_classes)
+    add_schema(g, spec_id, base_url, created, modified, conf_classes)
+
+    print("Spreadsheet Graph: " + str(g.serialize(format="turtle"), 'utf-8'))
+
+    if not replace:
+        stored_spec_file = inputDir + "/" + spec_id + ".ttl"
+
+        g2 = rdflib.Graph()
+        if os.path.exists(stored_spec_file):
+            g2.parse(stored_spec_file, format='ttl')
+
+
+        # load_matrix(query, gs_range, gs_sheet_name):)
+        for s, p, o in g2:
+            print(p)
+            print(g.value(s, p))
+            print((s, p, None) in g)
+            if not (s, p, None) in g:
+                g.add((s, p, o))
+        print("Merged Graph: " + str(g.serialize(format="turtle"), 'utf-8'))
+
     formatted_ttl: str = str(g.serialize(format="turtle"))
-    print(formatted_ttl)
-    with open(outputDir + "/" + spec_id + "_" + spreadsheetId + ".ttl", 'w') as fout_ttl:
+    # print(formatted_ttl)
+    # with open(outputDir + "/" + spec_id + "_" + spreadsheetId + ".ttl", 'w') as fout_ttl:
+    with open(outputDir + "/" + spec_id + ".ttl", 'w') as fout_ttl:
         fout_ttl.write(formatted_ttl + "    ")
         fout_ttl.close()
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-i",
+        "--input",
+        help="specification rdf base directory",
+    )
+
+    parser.add_argument(
+        "-d",
         "--id",
         help="Spreadsheet ID",
     )
@@ -152,9 +256,18 @@ if __name__ == '__main__':
         "--output",
         help="Output directory",
     )
+
+    parser.add_argument(
+        "-r",
+        "--replace",
+        const=True, nargs='?',
+        help="Replace old file instead of overwriting values/ontology objects"
+    )
+
     args = parser.parse_args()
     spreadsheetId = None
     outputDir = None
+    replace = False
     if args.url:
         # s = args.url.strip('https://docs.google.com/spreadsheets/d/')
         ss = args.url.split('/')
@@ -163,5 +276,9 @@ if __name__ == '__main__':
         spreadsheetId = args.id
     if args.output:
         outputDir = args.output
+    if args.input:
+        inputDir = args.input
+    if args.replace:
+        replace = args.replace
 
-    main()
+    main(replace)
