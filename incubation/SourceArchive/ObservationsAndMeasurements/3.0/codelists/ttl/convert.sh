9d749fa3493fe0da7849fab99a653cbc0469faef
Gobe Hobona <ghobona@opengeospatial.org>
2022-02-18 11:41:44 +0000

    tidying up Incubation folder

--- incubation/SourceArchive/ObservationsAndMeasurements/3.0/codelists/ttl/convert.sh
+++ incubation/SourceArchive/ObservationsAndMeasurements/3.0/codelists/ttl/convert.sh
@@ -4,3 +4,5 @@
 /Users/ogckm/Tools/apache-jena-4.1.0/bin/riot --out=Turtle /Users/ogckm/Downloads/codelists/observationTypeByResultType.jsonld > observationTypeByResultType.ttl
 /Users/ogckm/Tools/apache-jena-4.1.0/bin/riot --out=Turtle /Users/ogckm/Downloads/codelists/sampleType.jsonld > sampleType.ttl
 /Users/ogckm/Tools/apache-jena-4.1.0/bin/riot --out=Turtle /Users/ogckm/Downloads/codelists/sampleTypeByGeometryType.jsonld > sampleTypeByGeometryType.ttl
+
+# Now concatenate the generated files to create the om_2021.ttl file
