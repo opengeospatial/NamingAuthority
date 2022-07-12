1d8ec92ae78342a99b97446861cf8aa17c51d62f
Piotr Zaborowski <pzaborowski@ogc.org>
2022-07-12 10:30:38 +0200

    Features status update

---
+++ scripts/specification_model.py
@@ -0,0 +1,27 @@
+#to-be model generated from ModSpec
+
+def class Specification:
+    def __init__(self):
+        classes = ConformanceClass[]
+        classes = ConformanceClass[]
+
+    serialize
+
+def class ConformanceClass:
+    def __init__():
+        ConformanceTest tests[]
+        requirementsTested = (RequirementsClass)None
+
+def class RequirementsClass:
+    def __init__(self):
+        requirements = Requirement[];
+
+def class Requirement:
+    def __init__(self):
+        requirements = Requirement[];
+    Test test?
+
+def class ConformanceTest:
+    def __init__(self):
+        requirements = Requirement;
+     requirement?
