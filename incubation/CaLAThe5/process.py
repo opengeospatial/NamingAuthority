3ae4b34679a324dd78c2ea85330894a4be78e241
Gobe Hobona <ghobona@opengeospatial.org>
2022-08-03 15:25:13 +0100

    CaLAThe v5 codelist added

--- incubation/CaLAThe5/process.py
+++ incubation/CaLAThe5/process.py
@@ -24,23 +24,37 @@ for row in qres:
 
 h = rdflib.Graph()
 h.parse("/Users/ogckm/Documents/GitHub/NamingAuthority/incubation/CaLAThe5/CaLAThe5.rdf")
+h.parse("/Users/ogckm/Documents/GitHub/NamingAuthority/incubation/CaLAThe5/CaLATheCodeList2.rdf")
 
 
 # Now we compare v4 concepts to v5 concepts to determine which are being deprecated and which are also being superseded
 
-other_query = """
+v5_identifiers = []
+
+v5_concept_query = """
 SELECT * {
  ?s ?p ?o
  FILTER (isURI(?s) && STRSTARTS(str(?s), str('https://www.opengis.net/def/CaLAThe/5.0') ) )
 }"""
 
-v5_identifiers = []
+v5_concept_qres = h.query(v5_concept_query)
+for v5_concept_row in v5_concept_qres:
+    if not(str(v5_concept_row.s) in v5_identifiers):
+        v5_identifiers.append(str(v5_concept_row.s))
+        #print(f"{v5_concept_row.s} prefLabel {v5_concept_row.o}")
 
-other_qres = h.query(other_query)
-for other_row in other_qres:
-    if not(str(other_row.s) in v5_identifiers):
-        v5_identifiers.append(str(other_row.s))
-        #print(f"{other_row.s} prefLabel {other_row.o}")
+v5_codelist_query = """
+SELECT * {
+ ?s ?p ?o
+ FILTER (isURI(?s) && STRSTARTS(str(?s), str('https://www.opengis.net/def/CaLATheCodeList/5.0') ) )
+}"""
+
+v5_codelist_qres = h.query(v5_codelist_query)
+for v5_codelist_row in v5_codelist_qres:
+    if not(str(v5_codelist_row.s) in v5_identifiers):
+        v5_identifiers.append(str(v5_codelist_row.s))
+        #print(f"{v5_codelist_row.s} prefLabel {v5_codelist_row.o}")
+
 
 # Then we declare the v4 concepts deprecated and/or superseded. Note that a concept can be deprecated without being superseded
 
@@ -70,3 +84,5 @@ for v4_item in v4_identifiers:
 # we now write out v5 to TTL
 
 h.serialize(destination="20220802_CaLAThe5.ttl")
+
+#=================================================
