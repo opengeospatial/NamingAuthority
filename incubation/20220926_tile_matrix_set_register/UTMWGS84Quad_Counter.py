ba843c53ce158e47f449320911e15e00f3cab43f
Gobe Hobona <ghobona@opengeospatial.org>
2022-09-26 08:09:47 +0100

    created input csv file

---
+++ incubation/20220926_tile_matrix_set_register/UTMWGS84Quad_Counter.py
@@ -0,0 +1,2 @@
+for i in range(11,60):
+    print("http://www.opengis.net/def/tilematrixset/OGC/1.0/UTM"+str(i)+"WGS84Quad,Universal Transverse Mercator Zone "+str(i)+" WGS84 Quad,UTM"+str(i)+"WGS84Quad,2022-09-26")
