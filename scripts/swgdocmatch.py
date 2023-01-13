3c9f9204a0581fee97d65bec33883be621038ea1
Alejandro Villar <avillar@ogc.org>
2023-01-13 11:41:36 +0100

    Enable SWG - document matching workflow

--- scripts/swgdocmatch.py
+++ scripts/swgdocmatch.py
@@ -3,20 +3,24 @@
 import csv
 import requests
 import json
+import os
 from rdflib import Graph, URIRef, PROV
 
-GS_URL = 'https://docs.google.com/spreadsheets/d/1zBdwiAmodZttlMwlk9vUgVoJ5oroVpHuce_0rZ8pJp4/export?format=csv'
+GS_URL = os.environ['SWGDOCMATCH_GS_URL']
 SWG_FILE = 'incubation/working-groups/working-groups.json'
 
 BODIES = 'http://www.opengis.net/def/entities/bodies/'
 DOCS = 'http://www.opengis.net/def/docs/'
 
+print("Loading working groups file...")
 with open(SWG_FILE, 'rb') as f:
     swgs = {int(s['id']): s for s in json.load(f)}
 
+print("Downloading Google Spreadsheet...")
 r = requests.get(GS_URL)
 r.raise_for_status()
 
+print("Matching...")
 reader = csv.DictReader(r.text.splitlines())
 g = Graph()
 g.bind('bodies', BODIES)
@@ -31,4 +35,6 @@ for row in reader:
             # Not a numeric SWGid
             pass
 
+print("Writing output...")
 g.serialize('incubation/working-groups/docs.ttl', format='ttl')
+print("All tasks done")
